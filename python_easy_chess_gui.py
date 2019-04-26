82c0a2b42a82712551b7f4d64182e72c2f85282e
fsmosca <ferdinandmosca@gmail.com>
2019-04-26 19:02:45 +0800

    Update python_easy_chess_gui.py
    * Increase move list box.
    * Reduce engine search info box.

--- python_easy_chess_gui.py
+++ python_easy_chess_gui.py
@@ -390,7 +390,7 @@ def build_main_layout(is_user_white):
         [sg.Text('White', size=(6, 1), font=('Consolas', 10)), sg.InputText('', font=('Consolas', 10), key='_White_', size=(34, 1))],            
         [sg.Text('Black', size=(6, 1), font=('Consolas', 10)), sg.InputText('', font=('Consolas', 10), key='_Black_', size=(34, 1))],
         [sg.Text('MOVE LIST', font=('Consolas', 10))],            
-        [sg.Multiline([], do_not_clear=True, autoscroll=True, size=(40, 4), font=('Consolas', 10), key='_movelist_')],
+        [sg.Multiline([], do_not_clear=True, autoscroll=True, size=(40, 8), font=('Consolas', 10), key='_movelist_')],
         [sg.Text('ENGINE SEARCH INFO', font=('Consolas', 10))],
         [sg.Multiline([], do_not_clear=True, autoscroll=True, size=(40, 4), font=('Consolas', 10), key='_engineinfo_')],        
     ]
